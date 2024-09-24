2a86a3d62954dc05ed1e3a74b9972c0968be9093
Emanuele <Emanuele@LAB-EMANUELE>
2024-09-24 11:35:43 +0200

    camera

---
+++ ApplicazioneWebBlazorLogin/wwwroot/camera.js
@@ -0,0 +1,22 @@
+﻿window.initializeCamera = () => {
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then((stream) => {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch((err) => {
+            console.error('Errore nell\'accesso alla fotocamera:', err);
+        });
+
+    window.takePhoto = () => {
+        context.drawImage(video, 0, 0, 640, 480);
+    }
+
+    window.getCanvasData = () => {
+        return canvas.toDataURL();
+    }
+};
